4704a29cb65c87ed3223c6b2ce6f1a48e248df33
Juan Pires da Silva <152239991+pds-juan@users.noreply.github.com>
2024-03-05 22:59:45 -0300

    commit automação

---
+++ Aula 01/pegar_posicao.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+# descobrir a posição do click na tela
+
+time.sleep(5)
+print(pyautogui.position())
+
+# depois de rodar o código, colocar o mouse onde se quer clicar e esperar 5 segundos
